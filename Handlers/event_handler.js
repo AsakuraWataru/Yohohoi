882a305d91d38504a27c1dae818f4e572dc40e9e
Hikaru Hunter <102463088+AsakuraWataru@users.noreply.github.com>
2022-06-22 15:03:49 +0000

    Like before

--- Handlers/event_handler.js
+++ Handlers/event_handler.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 module.exports = (client, Discord) => {
   const load_dirs = (dirs) => {
-    const event_file = fs.readdirSync(`./Events/${dirs}`)
+    const event_file = fs.readdirSync(`./Events/${dirs}`).filter(file => file.endWith('.js'))
+
   }
 }
