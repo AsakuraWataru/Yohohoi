a9545d863ed8eabe14da872e81a590a76ad6776f
Hikaru Hunter <102463088+AsakuraWataru@users.noreply.github.com>
2022-07-16 12:18:35 +0000

    Done with some shit

---
+++ Command/countdown.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: 'countdown',
+  description: "Like it's name lol",
+  execute(client, message, args, Discord){
+    let time
+    if(args[1] === 'second'){
+      time = parseInt(args[0]) * 1000
+    }else if(args[1] === 'minute'){
+      time = parseInt(args[0]) * 1000 * 60
+    }else if(args[0] === 'hour'){
+      time = parseInt(args[0]) * 1000 * 60 * 60
+    }
+
+    console.log(`${time}`)
+    console.log(args[0])
+    
+    const aa = () => {
+      message.channel.send(`${message.author} hoàn tất đếm ngược`)
+    }
+    setTimeout(aa, time)
+       
+  },
+}
